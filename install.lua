382202f1a896c114dd964d9de202bdaccb2f4138
patryk3211 <patrykmierzy@gmail.com>
2022-07-25 00:08:11 +0200

    Config gen

--- install.lua
+++ install.lua
@@ -11,5 +11,16 @@ for i, file in ipairs(files) do
     shell.execute("wget", rootPath..file)
 end
 
+print("Files downloaded successfully")
+
 -- Create config dir
 fs.makeDir(shell.resolve("config"))
+
+-- Make example config
+local confFile = io.open(shell.resolve("config/mappings.conf"), "w+")
+confFile:write("# This file should contain the mappings for redstone signals in the following format:\n# <Mapping Name> <Peripheral ID>/<Side>:<Bit>\nexample redstoneIntegrator_0/left:0")
+confFile:close()
+
+print("Configs generated")
+
+print("Install complete")
