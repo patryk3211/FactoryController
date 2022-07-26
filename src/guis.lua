95cc493860ceafaffef2849de6223fe191d5dfb5
patryk3211 <patrykmierzy@gmail.com>
2022-07-26 13:55:48 +0200

    Width

--- src/guis.lua
+++ src/guis.lua
@@ -124,9 +124,9 @@ module.manualControl = {
         panel = { type = "panel", x = 1, y = 2, width = 17, height = 5, color = colors.white },
         chocolate_button = { type = "button", x = 1, y = 2, width = 11, height = 1, bg = colors.white, fg = colors.black, text = "Chocolate", handler = function (window) selectOutput("chocolate", window) end },
         caramel_button = { type = "button", x = 1, y = 3, width = 11, height = 1, bg = colors.white, fg = colors.black, text = "Caramel", handler = function (window) selectOutput("caramel", window) end },
-        white_chocolate_button = { type = "button", x = 1, y = 4, width = 11, height = 1, bg = colors.white, fg = colors.black, text = "White Chocolate", handler = function (window) selectOutput("white_chocolate", window) end },
-        dark_chocolate_button = { type = "button", x = 1, y = 5, width = 11, height = 1, bg = colors.white, fg = colors.black, text = "Dark Chocolate", handler = function (window) selectOutput("dark_chocolate", window) end },
-        hot_chocolate_button = { type = "button", x = 1, y = 6, width = 11, height = 1, bg = colors.white, fg = colors.black, text = "Hot Chocolate", handler = function (window) selectOutput("hot_chocolate", window) end }
+        white_chocolate_button = { type = "button", x = 1, y = 4, width = 17, height = 1, bg = colors.white, fg = colors.black, text = "White Chocolate", handler = function (window) selectOutput("white_chocolate", window) end },
+        dark_chocolate_button = { type = "button", x = 1, y = 5, width = 16, height = 1, bg = colors.white, fg = colors.black, text = "Dark Chocolate", handler = function (window) selectOutput("dark_chocolate", window) end },
+        hot_chocolate_button = { type = "button", x = 1, y = 6, width = 15, height = 1, bg = colors.white, fg = colors.black, text = "Hot Chocolate", handler = function (window) selectOutput("hot_chocolate", window) end }
     } },
     button_output_select = { type = "button", x = 21, y = 7, height = 3, width = 18, fg = colors.white, bg = colors.gray, text = "Output Tank", handler = function (window)
         window.output_tank_group.visible = true
