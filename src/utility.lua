6595556f864960bfda028e8ebb3954148af41273
patryk3211 <patrykmierzy@gmail.com>
2022-07-27 11:45:10 +0200

    New Timer

--- src/utility.lua
+++ src/utility.lua
@@ -1,81 +1,51 @@
 local module = {}
 
-local timerScheduled = {}
+local systemTimer = nil
 local timers = {}
-local systemTimer = 0
-local timerStartTime = 0
 
 function module.scheduleTimer(time, handler, ...)
-    print("Scheduling for "..(time * 20).." ticks")
-    if timers[1] == nil then
-        timers[1] = {
-            time = time * 20,
-            handlers = { { func = handler, args = {...} } }
-        }
-        systemTimer = os.startTimer(time)
-        timerStartTime = os.clock() * 20
-        return
-    else
-        -- Don't trust os.clock()
-        local clock = os.clock() * 20
-        local timerRunTime = math.floor(clock - timerStartTime)
-        timers[1].time = timers[1].time - timerRunTime
-        if timers[1].time < 0 then
-            timers[1].time = 0
-        end
-        timerStartTime = clock
-        print("Timer changed by "..timerRunTime.." new time "..timers[1].time)
+    local tickTime = math.floor(time * 20)
+
+    if systemTimer == nil then
+        systemTimer = os.startTimer(0.05)
     end
 
-    local timeLeft = time * 20
+    local timeLeft = tickTime
     for i = 1, #timers do
         local timer = timers[i]
 
-        local saveTime = timeLeft
+        local _timeLeft = timeLeft
         timeLeft = timeLeft - timer.time
+
         if timeLeft == 0 then
-            -- Append to handler list
-            timer.handlers[#timer.handlers+1] = { func = handler, args = {...} }
-            print("Added to handler list")
+            -- Append handler
+            table.insert(timer.handlers, { func = handler, args = {...} })
             return
         elseif timeLeft < 0 then
-            timer.time = timer.time - saveTime
-
-            -- Insert an event
-            local newTimer = {
-                time = saveTime,
-                handlers = { { func = handler, args = {...} } }
-            }
-            table.insert(timers, i, newTimer)
-            if i == 1 then
-                os.cancelTimer(systemTimer)
-                systemTimer = os.startTimer(time)
-            end
-            print("Timer inserted")
+            -- Insert before this timer
+            table.insert(timers, i, { time = timeLeft, handlers = { { func = handler, args = {...} } } })
             return
         end
     end
-    timers[#timers+1] = {
-        time = time * 20,
-        handlers = { { func = handler, args = {...} } }
-    }
-    print("Added at the end")
+
+    table.insert(timers, { time = timeLeft, handlers = { { func = handler, args = {...} } } })
 end
 
 function module.handleTimerEvent(eventData)
     if eventData[2] ~= systemTimer then
         return
     end
-    local timer = table.remove(timers, 1)
-    print("Firing after "..timer.time.." ticks, actual time "..(os.clock() * 20 - timerStartTime))
-    for i = 1, #timer.handlers do
-        local handler = timer.handlers[i]
-        handler.func(table.unpack(handler.args))
+
+    local firstTimer = timers[1]
+    firstTimer.time = firstTimer.time - 1
+    if firstTimer.time == 0 then
+        for i = 1, #firstTimer.handlers do
+            local handler = firstTimer.handlers[i]
+            handler.func(table.unpack(handler.args))
+        end
     end
-    if timers[1] ~= nil then
-        systemTimer = os.startTimer(timers[1].time / 20)
-        timerStartTime = os.clock() * 20
-    end
+
+    systemTimer = os.startTimer(0.05)
 end
 
 return module
